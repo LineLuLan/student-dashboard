1db0aefadfaca19cfc9dd21f8bd67c185faa87fb
bodkiemviec <tranbaominhbadao@gmail.com>
2026-02-15 12:25:23 +0700

    Initial chart on dashboard

---
+++ web/charts/scatter.js
@@ -0,0 +1,144 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+
+console.log("SCATTER FILE LOADED");
+
+function linearRegression(data) {
+    const n = data.length;
+
+    const sumX = d3.sum(data, d => d.hours_studied);
+    const sumY = d3.sum(data, d => d.exam_score);
+    const sumXY = d3.sum(data, d => d.hours_studied * d.exam_score);
+    const sumX2 = d3.sum(data, d => d.hours_studied * d.hours_studied);
+
+    const slope = (n * sumXY - sumX * sumY) /
+                  (n * sumX2 - sumX * sumX);
+
+    const intercept = (sumY - slope * sumX) / n;
+
+    return { slope, intercept };
+}
+
+export function drawScatter(data) {
+    console.log("drawScatter RUNNING", data.length);
+    
+    d3.select("#scatter").selectAll("*").remove(); // VERY IMPORTANT
+
+    const width = 600;
+    const height = 300;
+    const margin = {top:40, right:40, bottom:60, left:60};
+
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const svg = d3.select("#scatter")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    const g = svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // X scale
+    const x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.hours_studied))
+        .range([0, innerWidth])
+        .nice();
+
+    // Y scale
+    const y = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.exam_score))
+        .range([innerHeight, 0])
+        .nice();
+
+    const color = d3.scaleOrdinal()
+        .domain(["low", "medium", "high"])
+        .range(["#e74c3c", "#f1c40f", "#2ecc71"]);
+
+    g.append("g")
+        .attr("transform", `translate(0,${innerHeight})`)
+        .call(d3.axisBottom(x));
+
+    g.append("g")
+        // .attr("transform", `translate(${innerWeight})`)
+        .call(d3.axisLeft(y));
+
+    const tooltip = d3.select("#tooltip");  
+        
+    g.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", d => x(d.hours_studied))
+    .attr("cy", d => y(d.exam_score))
+    .attr("r", 4)
+    .attr("fill", d => color(d.motivation))
+    .attr("opacity", 0.7)
+    .on("mouseover", function(event, d) {
+
+        d3.select(this)
+            .attr("r", 7)
+            .attr("stroke", "black");
+
+        tooltip
+            .style("opacity", 1)
+            .html(`
+                <strong>Score:</strong> ${d.exam_score}<br>
+                <strong>Hours:</strong> ${d.hours_studied}<br>
+                <strong>Motivation:</strong> ${d.motivation}<br>
+                <strong>Attendance:</strong> ${d.attendance}%
+            `);
+    })
+    .on("mousemove", function(event) {
+        tooltip
+            .style("left", (event.pageX + 15) + "px")
+            .style("top", (event.pageY - 20) + "px");
+    })
+    .on("mouseout", function() {
+
+        d3.select(this)
+            .attr("r", 4)
+            .attr("stroke", "none");
+
+        tooltip.style("opacity", 0);
+    });
+
+    // ===== REGRESSION =====
+    const { slope, intercept } = linearRegression(data);
+
+    // Lấy đúng domain hiện tại
+    const [xMin, xMax] = x.domain();
+
+    // Tính y tương ứng
+    const yMin = slope * xMin + intercept;
+    const yMax = slope * xMax + intercept;
+
+    // Nếu regression vượt ngoài domain thì mở rộng domain
+    const yDomain = y.domain();
+
+    y.domain([
+        Math.min(yDomain[0], yMin, yMax),
+        Math.max(yDomain[1], yMin, yMax)
+    ]);
+
+    // Cập nhật lại axis Y
+    g.selectAll(".y-axis").remove();
+
+    g.append("g")
+        .attr("class", "y-axis")
+        .call(d3.axisLeft(y));
+
+    // Vẽ line
+    g.append("line")
+        .attr("x1", x(xMin))
+        .attr("y1", y(yMin))
+        .attr("x2", x(xMax))
+        .attr("y2", y(yMax))
+        .attr("stroke", "red")
+        .attr("stroke-width", 3);
+
+    console.log("Slope:", slope);
+    console.log("Intercept:", intercept);
+
+}
+
+
---
+++ web/main.js
@@ -0,0 +1,56 @@
+console.log("Before import");
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import { drawScatter } from "./charts/scatter.js";
+console.log("After import");
+
+let fullData = [];
+
+d3.csv("data/clean_student_performance.csv", d => ({
+    hours_studied: +d.hours_studied,
+    attendance: +d.attendance,
+    parental_involvement: d.parental_involvement,
+    resources: d.resources,
+    extracurricular: d.extracurricular,
+    sleep_hours: +d.sleep_hours,
+    previous_scores: +d.previous_scores,
+    motivation: d.motivation,
+    internet: d.internet,
+    tutoring: +d.tutoring,
+    family_income: d.family_income,
+    teacher_quality: d.teacher_quality,
+    school_type: d.school_type,
+    peer_influence: d.peer_influence,
+    physical_activity: +d.physical_activity,
+    learning_disability: d.learning_disability,
+    parent_education: d.parent_education,
+    distance: d.distance,
+    gender: d.gender,
+    exam_score: +d.exam_score
+}))
+.then(data => {
+    console.log("CSV LOADED", data.length);
+    console.log("parsed:", data[0]);
+    drawScatter(data);
+    fullData = data;
+    update();
+});
+
+function getFilters() {
+    return Array.from(document.querySelectorAll(".controls input:checked"))
+        .map(cb => cb.value);
+}
+
+function update() {
+    const selected = getFilters();
+
+    console.log("filters:", selected);
+
+    const filtered = fullData.filter(d => selected.includes(d.motivation));
+
+    console.log("filtered size:", filtered.length);
+
+    drawScatter(filtered);
+}
+
+document.querySelectorAll(".controls input")
+    .forEach(cb => cb.addEventListener("change", update));
